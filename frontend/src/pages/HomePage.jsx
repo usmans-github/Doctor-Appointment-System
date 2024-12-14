8f3599faf7dbe975bf32df362379a7a4c0ac361a
M Usman <usmanali730771@gmail.com>
2024-12-14 18:20:28 +0500

    homepage hero wection

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -24,7 +24,8 @@ const HomePage = () => {
 
 
   return (
-    <section className="bg-indigo-500 px-6 md:px-20 py-12">
+    
+    <section className="bg-indigo-500 px-6 md:px-20 py-12 min-h-[86.2vh]">
       <div className="flex flex-col md:flex-row items-center justify-between">
         <div className="text-white mb-8 md:mb-0 md:max-w-xl">
           <h1 className="text-4xl md:text-5xl font-bold mb-4">
@@ -40,8 +41,8 @@ const HomePage = () => {
           <button type="button" className="text-indigo-500 bg-white hover:bg-indigo-50 focus:ring-4 focus:ring-blue-300 
           font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Book appointment →</button>
         </div>
-        <div className="relative w-full md:w-1/2 h-[300px] md:h-[400px]">
-       <img src="../assets/we" alt="" />
+        <div className="relative w-full md:w-1/2  md:h-[400px]">
+        <img src="/doctors.png" alt="" className=''/>
         </div>
       </div>
     </section>
