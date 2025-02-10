03cf3512f445aed450f199d6d125b95e74032c15
usmans-github <usmanali730771@gmail.com>
2025-02-10 23:40:02 +0500

    refactor Featured component for improved image responsiveness and text styling

--- frontend/src/components/Featured.jsx
+++ frontend/src/components/Featured.jsx
@@ -7,17 +7,17 @@ const Featured = () => {
         <div className='grid grid-cols-2 gap-0'>
             {/* Image */}
             <div className="p-6 ">
-            <img src="doctors.png" alt="doctors" className='md:h-[50vh] group-hover:scale-110 transition-all 
-               object -cover object-center bg-indigo-500  border rounded-[2.5rem]' />    
+            <img src="doctors.png" alt="doctors" className='md:h-[50vh] sm:h-[30vh] group-hover:scale-110 transition-all 
+               object -cover object-center bg-indigo-500  border rounded-3xl md:rounded-[2.5rem]' />    
             </div>
             {/* text */}
             <div className='flex flex-col items-start text-sm gap-0  md:gap-4 md:h-[60vh] justify-center'>
-            <p className='md:text-lg text-sm font-semibold'>Lorem, ipsum.</p>
-            <h1 className="md:text-5xl  text-start font-extrabold group-hover:text-zinc-900
+            <p className='md:text-lg font-semibold'>Lorem, ipsum.</p>
+            <h1 className="md:text-5xl px-2 text-start font-extrabold group-hover:text-zinc-900
               text-indigo-500 mt-2 ">
             Qualified and Experts Doctors that meet your needs
             </h1>
-            <p className='md:text-lg text-sm font-semibold'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate
+            <p className='hidden md:text-lg font-semibold'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate
                <br /> sequi officiis quo consectetur.</p>
             <div className='flex justify-center items-center group-hover:text-zinc-900 group-hover:gap-2 transition-all
              gap-1 md:mt-5  md:text-xl font-semibold  text-indigo-500'>
