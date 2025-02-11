9db1ab8d5fe5f8e70df7621b26d2d20d086bd7e0
usmans-github <usmanali730771@gmail.com>
2025-02-11 11:18:41 +0500

    refactor Featured component for improved layout and responsiveness

--- frontend/src/components/Featured.jsx
+++ frontend/src/components/Featured.jsx
@@ -4,16 +4,16 @@ import React from 'react'
 const Featured = () => {
   return (
     <section className='group  bg-[#f0f0f0] mt-16 border rounded-3xl md:rounded-[2.5rem] mx-2 md:mx-16'>
-        <div className='grid grid-cols-2 gap-0'>
+        <div className='grid grid-cols-2'>
             {/* Image */}
-            <div className="p-6 ">
-            <img src="doctors.png" alt="doctors" className='md:h-[50vh] sm:h-[30vh] group-hover:scale-110 transition-all 
-               object -cover object-center bg-indigo-500  border rounded-3xl md:rounded-[2.5rem]' />    
+            <div className="p-6 flex items-center">
+            <img src="doctors.png" alt="doctors" className='lg:h-[45vh] md:h-[50vh] sm:h-[30vh] md:mx-8 group-hover:scale-110 transition-all 
+               object-cover object-center bg-indigo-500  border rounded-3xl md:rounded-[2.5rem]' />    
             </div>
             {/* text */}
-            <div className='flex flex-col items-start text-sm gap-0  md:gap-4 md:h-[60vh] justify-center'>
+            <div className='flex flex-col items-start text-sm gap-0 md:gap-4 lg:h-[60vh] md:h-[50vh] justify-center'>
             <p className='md:text-lg font-semibold'>Lorem, ipsum.</p>
-            <h1 className="md:text-5xl px-2 text-start font-extrabold group-hover:text-zinc-900
+            <h1 className="lg:text-5xl md:text-3xl px-2 text-start font-extrabold group-hover:text-zinc-900
               text-indigo-500 mt-2 ">
             Qualified and Experts Doctors that meet your needs
             </h1>
