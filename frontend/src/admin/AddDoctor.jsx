50013a9498550eb118d4dc527e18e476ec96932e
Muhammad <your_correct_email@example.com>
2025-02-25 15:48:52 +0000

    doctor layout fix

--- frontend/src/admin/AddDoctor.jsx
+++ frontend/src/admin/AddDoctor.jsx
@@ -14,8 +14,6 @@ const AddDoctor = () => {
   const { register, handleSubmit, reset, watch } = useForm();
 
   const onSubmit = async (data) => {
-   
-
     const formData = new FormData();
     formData.append("name", data.name);
     formData.append("email", data.email);
@@ -28,9 +26,9 @@ const AddDoctor = () => {
 
     try {
       // console.log("form data is:", formData.data);
-      setloading(true)
+      setloading(true);
       const res = await axios.post(
-        `http://sehatx.com/api/admin/add-doctor`,
+        `https://sehatx.com/api/admin/add-doctor`,
         formData
       );
       if (res.data.success) {
@@ -137,76 +135,92 @@ const AddDoctor = () => {
                   />
                 </div>
 
-                {/* Right Column */}
-                <div className="space-y-4">
-                  {/* Phone */}
-                  <div>
-                    <label
-                      htmlFor="phone"
-                      className="block mb-2 text-sm font-medium text-gray-900"
-                    >
-                      Phone
-                    </label>
-                    <input
-                      {...register("phone")}
-                      type="number"
-                      id="phone"
-                      placeholder="0000000000"
-                      className="w-full bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-indigo-500 focus:border-indigo-500 p-2.5"
-                      required
-                    />
-                  </div>
-                  {/* Specialization */}
-                  <div>
-                    <label
-                      htmlFor="specialization"
-                      className="block mb-2 text-sm font-medium text-gray-900"
-                    >
-                      Specialization
-                    </label>
-                    <input
-                      {...register("specialization")}
-                      type="text"
-                      id="specialization"
-                      placeholder="Specialize In"
-                      className="w-full bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-indigo-500 focus:border-indigo-500 p-2.5"
-                      required
-                    />
-                  </div>
-                  {/* Experience */}
-                  <div>
-                    <label
-                      htmlFor="experience"
-                      className="block mb-2 text-sm font-medium text-gray-900"
-                    >
-                      Experience
-                    </label>
-                    <input
-                      {...register("experience")}
-                      type="text"
-                      id="experience"
-                      placeholder="Experience"
-                      className="w-full bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-indigo-500 focus:border-indigo-500 p-2.5"
-                      required
-                    />
-                  </div>
-                  {/* Fee */}
-                  <div>
-                    <label
-                      htmlFor="fee"
-                      className="block mb-2 text-sm font-medium text-gray-900"
-                    >
-                      Fee
-                    </label>
-                    <input
-                      {...register("fee")}
-                      type="number"
-                      id="fee"
-                      placeholder="0000"
-                      className="w-full bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-indigo-500 focus:border-indigo-500 p-2.5"
-                      required
-                    />
-                  </div>
+                {/* Phone */}
+                <div>
+                  <label
+                    htmlFor="phone"
+                    className="block mb-2 text-sm font-medium text-gray-900"
+                  >
+                    Phone
+                  </label>
+                  <input
+                    {...register("phone")}
+                    type="tel"
+                    id="phone"
+                    className="bg-white border border-indigo-500 text-gray-900 text-sm rounded-[1rem] focus:ring-indigo-500 focus:border-indigo-500 block w-full p-2.5"
+                    placeholder="123456789"
+                    required
+                  />
+                </div>
+
+                {/* Specialization */}
+                <div>
+                  <label
+                    htmlFor="specialization"
+                    className="block mb-2 text-sm font-medium text-gray-900"
+                  >
+                    Specialization
+                  </label>
+                  <input
+                    {...register("specialization")}
+                    type="text"
+                    id="specialization"
+                    className="bg-white border border-indigo-500 text-gray-900 text-sm rounded-[1rem] focus:ring-indigo-500 focus:border-indigo-500 block w-full p-2.5"
+                    placeholder="Specialization"
+                    required
+                  />
+                </div>
+
+                {/* Experience */}
+                <div>
+                  <label
+                    htmlFor="experience"
+                    className="block mb-2 text-sm font-medium text-gray-900"
+                  >
+                    Experience (Years)
+                  </label>
+                  <input
+                    {...register("experience")}
+                    type="number"
+                    id="experience"
+                    className="bg-white border border-indigo-500 text-gray-900 text-sm rounded-[1rem] focus:ring-indigo-500 focus:border-indigo-500 block w-full p-2.5"
+                    placeholder="Years of Experience"
+                    required
+                  />
+                </div>
+
+                {/* Fee */}
+                <div>
+                  <label
+                    htmlFor="fee"
+                    className="block mb-2 text-sm font-medium text-gray-900"
+                  >
+                    Consultation Fee
+                  </label>
+                  <input
+                    {...register("fee")}
+                    type="number"
+                    id="fee"
+                    className="bg-white border border-indigo-500 text-gray-900 text-sm rounded-[1rem] focus:ring-indigo-500 focus:border-indigo-500 block w-full p-2.5"
+                    placeholder="Fee in Rs"
+                    required
+                  />
+                </div>
+
+                {/* Upload Picture */}
+                <div>
+                  <label
+                    htmlFor="file"
+                    className="block mb-2 text-sm font-medium text-gray-900"
+                  >
+                    Upload Picture
+                  </label>
+                  <input
+                    {...register("file")}
+                    type="file"
+                    id="file"
+                    className="bg-white border border-indigo-500 text-gray-900 text-sm rounded-[1rem] focus:ring-indigo-500 focus:border-indigo-500 block w-full p-2.5"
+                  />
                 </div>
 
                 {/* Submit Button */}
