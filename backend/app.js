8cdec2ee3bfc870f855ff398fe5b7d58cde8e428
usmans-github <usmanali730771@gmail.com>
2025-02-25 20:56:28 +0500

    feat: enable CORS with credentials support in the backend

--- backend/app.js
+++ backend/app.js
@@ -16,7 +16,7 @@ connectDb();
 
 //Middlewares
 app.use(express.json());
-app.use(cors());  
+app.use(cors({credentials: true}));  
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
